da2b22266fb346424053f67cd0d60abeedf8ae4d
Victor Vasconcelos <victorolimpiovasconcelos0808@gmail.com>
2025-03-24 20:05:07 +0000

    feat: import the function to configure routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppRoutes } from "./routes/routes";
 
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
+    <AppRoutes />
   </React.StrictMode>,
 );
